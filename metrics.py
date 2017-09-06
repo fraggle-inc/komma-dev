6724dc8e7300f10e953ee841d92f30aefed8f35d
Mikkel Hartmann Jensen <mikkelheart@gmail.com>
2017-09-06 13:40:13 +0200

    First version of PR/ROC analysis done. Currently treats it as a multi-task problem, so each word is treated as its own classification problem. However, the figure includes the average across all of the words. Later I will make a more refined analysis, but the averages should be fine as rough guiding metrics.

--- metrics.py
+++ metrics.py
@@ -18,3 +18,56 @@ def plot_learning_curves(history):
         item.set_fontsize(20)
     print('Final training loss  ', history.history['loss'][-1])
     print('Final validation loss', history.history['val_loss'][-1])
+
+def ticks_and_gridlines(ax):
+    ax.set(xlim=[0.0, 1.0], ylim=[0.0, 1.0])
+    ax.set(xticks=np.arange(0,1,0.05), yticks=np.arange(0,1,0.05))
+    ax.grid(color='gray', linestyle='--', linewidth=.5, which='major');
+
+def collapse_multi_task_to_single_metric(y_hat, y):
+    """
+        The output of the model is multidimensional. We need to compress this into 
+        a single value, so we can do precision and recall analysis.
+    """
+    return y_hat, y
+
+def make_roc_pr_plot(y_hat, y):
+    fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(28,14))
+
+    # Initializing variables that will be updated in the loop
+    aggregate_tpr = 0.0
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    aggregate_roc_auc = 0
+    aggregate_pr_auc = 0
+
+    for idx in range(y_hat.shape[1]):
+        # Calculating false and true positive rate
+        fpr, tpr, threshold = roc_curve(y[:, idx], y_hat[:, idx])
+        roc_auc = auc(fpr, tpr)
+        aggregate_tpr += np.interp(mean_fpr, fpr, tpr)
+        
+        # Calculating precision and recall
+        precision, recall, threshold = precision_recall_curve(y[:, idx], y_hat[:, idx]);
+        pr_auc = auc(recall, precision)
+        aggregate_pr_auc += pr_auc
+
+        # Plotting the metrics
+        ax0.plot(fpr, tpr, 'ob-')
+        ax1.plot(recall, precision, 'ob-')
+    
+    # Plotting the average metrics
+    mean_tpr = np.divide(aggregate_tpr, (idx+1))
+    mean_roc_auc = metrics.auc(mean_fpr, mean_tpr)
+    mean_pr_auc = aggregate_pr_auc/(idx+1)
+
+    ax0.plot(mean_fpr, mean_tpr, lw=3, color='k', label='Average recall ({0:.2f})'.format(mean_roc_auc))
+    ax1.plot(1,1, lw=3, color='k', label='Mean PR AUC ({0:.2f})'.format(mean_pr_auc))
+
+    # Styling the ticks and gridlines
+    ticks_and_gridlines(ax0)
+    ticks_and_gridlines(ax1)
+    # Adding legends
+    ax0.legend()
+    ax1.legend()
+    plt.show()
